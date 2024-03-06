cced3da0493908e9b7a1679735ab6631205fca49
mylong123 <52267566+mylong123@users.noreply.github.com>
2024-03-06 12:02:15 +0800

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -11,4 +11,52 @@ $(document).ready(function() {
 		$(".TopButtons").slideUp()
 	});
 
+
 });
+
+
+//懒加载
+window.onload = function(){
+    var scrollTop = window.scrollY;
+    var imgs = Array.from(document.querySelectorAll('.cover-full'));
+    lazyLoad();
+    // 采用了节流函数
+    window.addEventListener('scroll',throttle(lazyLoad,500,1000));
+
+    function throttle(fun, delay, time) {
+        var timeout,
+            startTime = new Date();
+        return function() {
+
+            var context = this,
+                args = arguments,
+                curTime = new Date();
+            clearTimeout(timeout);
+            // 如果达到了规定的触发时间间隔，触发 handler
+            // console.log(curTime - startTime)
+            if (curTime - startTime >= time) {
+                fun();
+                startTime = curTime;
+                // 没达到触发间隔，重新设定定时器
+            } else {
+                timeout = setTimeout(fun, delay);
+            }
+        };
+    };
+    // 实际想绑定在 scroll 事件上的 handler
+    // 需要访问到imgs ,  scroll 
+    function lazyLoad(){
+        scrollTop = window.scrollY;
+        imgs.forEach((item,index)=>{
+            if( scrollTop===0 && item.dataset.src !== '' && item.offsetTop < window.innerHeight + scrollTop ){
+                // alert()
+                item.setAttribute('src',item.dataset.src)
+                item.setAttribute('data-src','')
+            }else if( item.dataset.src !== '' && item.offsetTop < window.innerHeight + scrollTop && item.offsetTop > scrollTop ){
+                item.setAttribute('src',item.dataset.src)
+                item.setAttribute('data-src','')
+            }
+        })
+    };
+
+}
